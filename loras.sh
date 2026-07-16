ac913160a4c3a6f25a0a083e3dda6933e7311273
helsingvash <tomoya-kun@live.com>
2026-07-16 12:52:40 -0300

    Add files via upload

---
+++ loras.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+DESTINO="$HOME/SwarmUI/Models/Lora"
+mkdir -p "$DESTINO"
+
+echo "Cole a URL ou apenas o domínio do Cloudflare Tunnel:"
+read -rp "> " HOST
+
+HOST="${HOST#https://}"
+HOST="${HOST#http://}"
+HOST="${HOST%/}"
+
+BASE_URL="https://$HOST"
+
+echo "Obtendo lista de arquivos..."
+
+curl -s "$BASE_URL/" \
+| grep -oE 'href="[^"]+\.safetensors"' \
+| cut -d'"' -f2 \
+| while read -r arquivo; do
+    echo "Baixando $arquivo..."
+    wget -c -P "$DESTINO" "$BASE_URL/$arquivo"
+done
+
+echo "Concluído!"
